70ecfba8b60faa911607af121238d546ac4be234
kifangamukundi <pkmymcmbnhs@gmail.com>
2022-06-08 12:28:30 +0300

    Modified the user model

--- models/User.js
+++ models/User.js
@@ -2,10 +2,24 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
     email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    isSuperUser: {
+      type: Boolean,
+      default: false,
+    },
     isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isStaff: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
       type: Boolean,
       default: false,
     },
