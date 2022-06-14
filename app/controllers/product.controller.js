1050bcca481730d885a0edd195be799dc306f119
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2022-06-14 03:36:03 +0300

    changed a couple of things

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -121,7 +121,7 @@ exports.updateProduct = (req, res, next) => {
 
 exports.deleteProduct = (req, res, next) => {
   const id = req.params.productId;
-  Product.remove({ _id: id })
+  Product.deleteOne({ _id: id })
     .exec()
     .then(result => {
       res.status(200).json({
