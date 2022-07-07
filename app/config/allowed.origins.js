3995a4fe675ffc84eafe04698201d9f861db2d63
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2022-07-08 00:13:03 +0300

    changed the cors

--- app/config/allowed.origins.js
+++ app/config/allowed.origins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'http://www.synergisticagribusiness.com/',
+    'https://www.synergisticagribusiness.com/',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000'
