9addb36689367e8efaea2a094ce4d4972cc849b7
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2022-07-08 01:25:42 +0300

    Cors

--- app/config/cors.options.js
+++ app/config/cors.options.js
@@ -2,10 +2,15 @@ const allowedOrigins = require('./allowed.origins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if ((allowedOrigins.indexOf(origin) !== -1) && ('OPTIONS' == req.method)) {
+        if (allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
+        }
+        if ('OPTIONS' == req.method) {
+            res.sendStatus(200);
+          } else {
+            next();
         }
     },
     optionsSuccessStatus: 200
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 3500;
 // Connect to MongoDB
 connectDB();
 
+// preflight
+app.options('*', cors(corsOptions));
 // Cross Origin Resource Sharing
 app.use(cors(corsOptions));
 
