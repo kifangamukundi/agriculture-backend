8ab2d572b4a34bc91ca48d2b331a6af5ef614370
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2022-07-08 20:15:00 +0300

    reversed to original

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -2,13 +2,8 @@ const { verifySignUp } = require("../middlewares");
 const controller = require("../controllers/auth.controller");
 
 module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
+  app.options('*', cors()) // include before other routes
+  console.log("auth.routes called");
 
   app.post(
     "/api/auth/signup",
