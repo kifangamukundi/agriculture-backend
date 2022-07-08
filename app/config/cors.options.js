3d9e7d2891ca2b7378cc2e68a299ad70042af232
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2022-07-08 07:33:32 +0300

    reversed to original

--- app/config/cors.options.js
+++ app/config/cors.options.js
@@ -2,28 +2,13 @@ const allowedOrigins = require('./allowed.origins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    allowedHeaders: ["Origin", "Access-Control-Allow-Origin", "Content-Type",
-    "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-    "Access-Control-Request-Method", "Access-Control-Request-Headers"],
-    exposedHeaders: ["Origin", "Content-Type", "Accept", "Authorization",
-    "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-    preflightContinue: true,
     optionsSuccessStatus: 200
 }
-
-// options for localhost
-// if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//     callback(null, true)
-// } else {
-//     callback(new Error('Not allowed by CORS'));
-// }
 
 module.exports = corsOptions;
