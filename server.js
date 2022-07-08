77846290b1210112babd6b31a6c3449399c8486e
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2022-07-08 04:17:23 +0300

    cors fix

--- server.js
+++ server.js
@@ -14,12 +14,23 @@ const PORT = process.env.PORT || 3500;
 connectDB();
 
 // Cross Origin Resource Sharing
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+
+  exposedHeaders: ['Content-Length','x-access-token'],
+
+  credentials: true,
+}));
 
 // parse requests of content-type - application/json
 app.use(express.json());
