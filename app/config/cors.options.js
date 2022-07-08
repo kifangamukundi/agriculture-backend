8a6181878d611d388bcf2e0fbad661baa92e4dbb
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2022-07-08 19:48:14 +0300

    reversed to original

--- app/config/cors.options.js
+++ app/config/cors.options.js
@@ -2,10 +2,11 @@ const allowedOrigins = require('./allowed.origins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
+            console.log('Not allowed by CORS here!!!!!!!!');
         }
     },
     optionsSuccessStatus: 200
