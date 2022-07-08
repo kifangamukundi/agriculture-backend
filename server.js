1eb4be80f4076a655c38238c6368321dde5560a6
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2022-07-08 06:14:54 +0300

    reversed to original

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to kifanga application." });
 });
 
+app.options('*', cors())
 // routes
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
