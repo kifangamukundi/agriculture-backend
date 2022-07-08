908c1ae2611f655fd4e4e6fd14f53d59f61009a2
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2022-07-08 08:53:53 +0300

    reversed to original

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ connectDB();
 
 // Cross Origin Resource Sharing
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://www.synergisticagribusiness.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "www.synergisticagribusiness.com/"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
